8e96640d4d9d23dec96fef3480adbd47b1df1cf4
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-06-21 21:07:01 +0100

    make individual pages, App.js for routes only

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,109 +1,31 @@
-import react from 'react';
 import React from 'react';
-import './App.css';
+import Transactions from "./Transactions.js";
+import Portfolio from "./Portfolio.js";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Menu/>
-    </div>
-  );
-}
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/transactions">
+            <Transactions />
+          </Route>
 
-function Menu() {
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
 
-  const transactions = [
-    {
-      depot: "ING Diba",
-      name: "AMC Entertainment",
-      ticker: "AMC",
-      category: "Stock"
-    },
-    {
-      depot: "Halifax",
-      name: "S&P 500",
-      ticker: "CSPX",
-      category: "ETF"
-    }
-
-  ]
-
-  return (
-    <div className="Menu">
-      <TransactionTable transactions={transactions}/>
-      <AddTransactionButton/>
-      <ViewPortfolioButton/>
-    </div>
-  )
-}
-
-class TransactionTable extends React.Component {
-  render() {
-    const rows = this.props.transactions.map(
-      (transaction) => {
-        return <TransactionRow
-          depot={transaction.depot}
-          name={transaction.name}
-          ticker={transaction.ticker}
-          category={transaction.category}
-          />
-      }
-    )
-    console.log(rows);
-
-    return (
-      <div className="transaction-table">
-        <table>
-          <thead>
-            <tr>
-              <th>Depot</th>
-              <th>Name</th>
-              <th>Ticker</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </table>
+        </Switch>
       </div>
-    );
-  }
-}
-
-function TransactionRow(props) {
-  return (
-    <tr>
-      <td>{props.depot}</td>
-      <td>{props.name}</td>
-      <td>{props.ticker}</td>
-      <td>{props.category}</td>
-    </tr>
-  )
-}
-
-class AddTransactionButton extends React.Component {
-  handleClick() {
-    console.log("adding transaction...");
-  }
-
-  render() {
-    return (
-        <button onClick={() => this.handleClick()}>Add Transaction</button>
-    );
-  }
-}
-
-class ViewPortfolioButton extends React.Component {
-  handleClick() {
-    console.log("Opening Portfolio...");
-  }
-
-  render() {
-    return (
-        <button onClick={() => this.handleClick()}>View Portfolio</button>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
