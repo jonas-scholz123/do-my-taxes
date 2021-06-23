857dec21617dd6caf1b53261762eb08ec395d1cd
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-06-24 00:49:58 +0100

    fetch portfolio historical data

--- src/api.py
+++ src/api.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from werkzeug.exceptions import MethodNotAllowed
 
 from transactions import TransactionHandler
+from portfolio import Portfolio
 
 transaction_handler = TransactionHandler()
+portfolio = Portfolio()
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -23,6 +26,18 @@ def get_open_transactions():
 @cross_origin()
 def get_closed_transactions():
     return transaction_handler.fetch_closed_transactions().to_json(orient="records")
+
+@app.route('/api/portfolio/history', methods=["GET"])
+@cross_origin()
+def get_portfolio_history():
+    start = request.args.get("start")
+    end = request.args.get("end")
+    print(f"fetching from {start} to {end}")
+    df = portfolio.history(start, end, groupby="category").round(2)
+    # for speed, limit to ~500 rows
+    n = max(1, int(df.shape[0]/500))
+    df = df.iloc[::n, :]
+    return df.to_json(orient="columns")
 
 if __name__ == '__main__':
     app.run(debug=True)
