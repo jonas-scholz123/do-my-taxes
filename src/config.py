1a1d4865bf2b194784ab5ef104076c76f7fb8b69
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-07-02 21:03:07 +0100

    add currency code file

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 paths = dict(
     db_fpath = "../db/taxes.db",
-    transactions_fpath = "../data/transactions.csv"
+    transactions_fpath = "../data/transactions.csv",
+    currency_codes = "../data/currencyCodes.csv"
 )
