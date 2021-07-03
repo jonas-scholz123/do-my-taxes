962590c3ec6dbff3b0c3f99cd574b2ef367007a0
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-07-04 00:50:36 +0100

    make reusable api component

---
+++ frontend/src/axiosConfig.jsx
@@ -0,0 +1,9 @@
+// First we need to import axios.js
+import axios from 'axios';
+// Next we make an 'instance' of it
+const instance = axios.create({
+// .. where we make our configurations
+    baseURL: 'http://localhost:5000/api/'
+});
+
+export default instance;
