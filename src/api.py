ee487dfd5328bcb19ffd21c9d012b1f77c9ece72
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-07-04 00:48:55 +0100

    Make endpoints for editing, deleting transactions

--- src/api.py
+++ src/api.py
@@ -15,17 +15,22 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/api/transactions/all', methods=["GET"])
 @cross_origin()
 def get_all_transactions():
-    return transaction_handler.fetch_all_transactions().to_json(orient="records")
+    return transaction_handler.fetch_all_transactions().sort_values("buy_date").to_json(orient="records")
 
 @app.route('/api/transactions/open', methods=["GET"])
 @cross_origin()
 def get_open_transactions():
-    return transaction_handler.fetch_open_transactions().to_json(orient="records")
+    return transaction_handler.fetch_open_transactions() \
+                              .sort_values("buy_date", ascending=False) \
+                              .to_json(orient="records")
 
 @app.route('/api/transactions/closed', methods=["GET"])
 @cross_origin()
 def get_closed_transactions():
-    return transaction_handler.fetch_closed_transactions().to_json(orient="records")
+    return transaction_handler.fetch_closed_transactions() \
+                              .sort_values("sell_date", ascending=False) \
+                              .to_json(orient="records")
+
 
 @app.route('/api/portfolio/history', methods=["GET"])
 @cross_origin()
@@ -40,7 +45,7 @@ def get_portfolio_history():
     return df.to_json(orient="columns")
 
 @app.route('/api/portfolio/snapshot/now', methods=["GET"])
-@cross_origin(supports_credentials=True)
+@cross_origin()
 def get_current_portfolio():
     portfolio.generate()
     df = portfolio.df.copy()
@@ -48,15 +53,35 @@ def get_current_portfolio():
     df.sort_values("current_value_gbp", inplace=True, ascending=False)
     return df.round(2).to_json(orient="records")
 
+@app.route("/api/transactions/<transaction_id>", methods=["GET", "DELETE"])
+@cross_origin()
+def single_transaction(transaction_id):
+    if request.method == "GET":
+        return transaction_handler.fetch_by_id(transaction_id).loc[0, :].to_json(orient="index")
+    if request.method == "DELETE":
+        transaction_handler.delete(transaction_id)
+        return jsonify({'transaction': request.json}), 200
+
 @app.route('/api/transactions/add', methods=["GET", "POST"])
 @cross_origin()
 def create_transaction():
     transaction = Transaction(**request.json["values"])
-    print("NEW TRANSACTION: ", transaction)
-    print("BUY DATE: ", transaction.buy_date)
-
     if transaction.valid:
         transaction_handler.insert_valid_transaction(transaction)
+        return jsonify({'transaction': request.json}), 201
+    # Unprocessable entity => 422
+    return jsonify({'errors': transaction.errors, 'transaction': request.json}), 422
+
+@app.route('/api/transactions/edit', methods=["POST"])
+@cross_origin()
+def edit_transaction():
+    received = request.json
+    transaction_id = received["id"]
+    del received["id"]
+    transaction = Transaction(**received)
+
+    if transaction.valid:
+        transaction_handler.edit_transaction(transaction_id, transaction)
         return jsonify({'transaction': request.json}), 201
     # Unprocessable entity => 422
     return jsonify({'errors': transaction.errors, 'transaction': request.json}), 422
