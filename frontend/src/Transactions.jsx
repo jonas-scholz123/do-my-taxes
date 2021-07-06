cc4aabc88dfda7e6a121c0f2cfcf820d5db6058e
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-07-06 22:49:09 +0100

    Refactor components

--- frontend/src/Transactions.jsx
+++ frontend/src/Transactions.jsx
@@ -23,7 +23,7 @@ function Transactions() {
           </div>
           <div class="flex justify-center block">
             <OpenTransactionsTable
-              apiURL= "http://localhost:5000/api/transactions/open"
+              apiURL="http://localhost:5000/api/transactions/open"
             />
           </div>
           <div class="flex justify-between items-center block">
@@ -31,210 +31,186 @@ function Transactions() {
           </div>
           <div class="flex justify-center">
             <ClosedTransactionsTable
-              apiURL= "http://localhost:5000/api/transactions/closed"
+              apiURL="http://localhost:5000/api/transactions/closed"
             />
           </div>
         </div>
       </div>
-      {/*<Footer/>*/}
     </div>
   );
 }
 
-class ClosedTransactionsTable extends React.Component {
-
-  constructor(props) {
-    // props: apiURL, mainButton, 
-    super(props);
-
-    this.state = {
-      error: null,
-      isLoaded: false,
-      transactions: [],
-      nrRows: 5,
-    };
-  }
-
-  componentDidMount() {
-    fetch(this.props.apiURL)
-    .then(res => res.json())
-    .then(
-      (result) => {
-        this.setState({
-          isLoaded: true,
-          transactions: result,
-        });
-      },
-
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        })
-      }
-    )
-  }
-
-  handleShowMore() {
-    this.setState(prevState => ({
-        nrRows: Math.min(prevState.nrRows + 10, prevState.transactions.length)
-    }));
-  }
-
-  handleRowClick(id) {
-    this.setState({
-      navigate: true,
-      navigateTo: id.toString()
-    })
-  }
-
-  render() {
-
-    const {navigateTo, navigate, error, isLoaded, transactions } = this.state;
-
-    const anyTransactions = transactions.length !== 0;
-
-    if (navigate) {
-      return <Navigate to={navigateTo} push={true}/>
-    }
-
-    if (error) {
-      return <div> Error; {error.message} </div>;
-    } else if (!isLoaded) {
-      return (
-        <div>
-          <ClipLoader loading={!isLoaded} size={150} />
-        </div>
-      )
-    }
-
-    const NoTransactionText = ({visible}) => {
-      if (!visible) {
-        return <div/>
-      }
-      return (
-        <h4 class="text-2xl font-semibold text-gray-400 pb-6">
-          You don't have any closed transactions
-        </h4>
-      )
-    }
-
-    const TableAndButtons = ({visible}) => {
-      if (!visible) {
-        return <div/>
-      }
-      return (
-        <div class="w-full">
-          <div class="w-full justify-center">
-            <Table
-              content={transactions}
-              nrRows={this.state.nrRows}
-              clickable={true}
-              keyHeader="id"
-              onClick={(id) => this.handleRowClick(id)}
-            />
-          </div>
-
-          <div class="py-6 flex justify-center">
-            <ShowMoreButton onClick={() => this.handleShowMore()} />
-          </div>
-        </div>
-      )
-    }
-
-    return (
-      <div>
-        <NoTransactionText visible={!anyTransactions}/>
-        <TableAndButtons visible={anyTransactions}/>
-      </div>
-    )
-  }
-}
-
-
 class OpenTransactionsTable extends React.Component {
 
   constructor(props) {
-    // props: apiURL, mainButton, 
+    // props: apiURL
     super(props);
 
     this.state = {
-      error: null,
-      isLoaded: false,
-      transactions: [],
-      nrRows: 10,
       modalOpen: false,
-      navigate: false,
-      navigateTo: ""
+      reload: false,
+      nrReloads: 0
     };
-  }
-
-  componentDidMount() {
-    this.loadTransactions()
-  }
-
-  handleRowClick(id) {
-    this.setState({
-      navigate: true,
-      navigateTo: id.toString()
-    })
-  }
-
-  loadTransactions() {
-    fetch(this.props.apiURL)
-    .then(res => res.json())
-    .then(
-      (result) => {
-        this.setState({
-          isLoaded: true,
-          transactions: result
-        });
-      },
-
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        })
-      }
-    )
-  }
-
-  handleShowMore() {
-    this.setState(prevState => ({
-        nrRows: Math.min(prevState.nrRows + 10, prevState.transactions.length)
-    }));
   }
 
   setModalOpen(isOpen) {
     this.setState({modalOpen: isOpen})
   }
 
+  reload() {
+    this.setState((state) => ({nrReloads: state.nrReloads + 1}))
+    console.log(this.state.nrReloads)
+  }
+
   render() {
 
-    const {navigateTo, navigate, error, isLoaded, transactions } = this.state;
-
-    const anyTransactions = transactions.length !== 0;
-
-    if (navigate) {
-      return <Navigate to={navigateTo} push={true}/>
+    const AddTransactionButton = ({setModalOpen}) => {
+      return(
+        <div class="w-1/3 h-12 flex justify-end">
+          <Button
+            text="Add Transaction"
+            main={true}
+            handleClick={() => setModalOpen(true)}
+          />
+        </div>
+      )
     }
 
-    if (error) {
-      return <div> Error; {error.message} </div>;
-    } else if (!isLoaded) {
-      return <div> Loading... </div>
+    const MissingAndButton = ({setModalOpen}) => {
+      return (
+        <div class="flex justify-center">
+          <div>
+            <h4 class="text-2xl font-semibold text-gray-400">
+              You don't have any open transactions
+            </h4>
+            <div class="flex justify-center py-2">
+              <Button
+                text="Add Transaction"
+                main={true}
+                handleClick={() => setModalOpen(true)}
+              />
+            </div>
+          </div>
+        </div>
+      )
     }
 
-    const TableAndButtons = ({visible}) => {
+    return (
+      <div class="w-full">
+          {/*We pass the key to reload the child component so that it fetches from api */}
+        <TransactionTable
+          apiURL={this.props.apiURL}
+          bottomRightContent={<AddTransactionButton setModalOpen={(bool) => this.setModalOpen(bool)}/>}
+          NoTransactionContent={<MissingAndButton setModalOpen={(bool) => this.setModalOpen(bool)}/>}
+          key={this.state.nrReloads}
+        />
+
+        <Modal
+          open={this.state.modalOpen}
+          setOpen={(bool) => this.setModalOpen(bool)}
+          loadTransactions={() => this.reload()}
+        />
+      </div>
+    )
+  }
+}
+
+const ClosedTransactionsTable = (props) => {
+
+    const NoTransactionText = () => {
+      return (
+        <div class="flex justify-center">
+          <div>
+            <h4 class="text-2xl font-semibold text-gray-400 pb-6">
+              You don't have any closed transactions
+            </h4>
+          </div>
+        </div>
+      )
+    }
+
+  return (
+    <TransactionTable
+      NoTransactionContent={<NoTransactionText />}
+      {...props}
+      refresh={true}
+    />
+  )
+}
+
+class TransactionTable extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      transactions: [],
+      navigate: false,
+      navigateTo: null,
+      isLoaded: false,
+      nrRows: 0,
+      hideShowMore: false,
+      error: null
+    }
+  }
+
+  handleRowClick(id) {
+    this.setState({
+      navigate: true,
+      navigateTo: id.toString()
+    })
+  }
+
+  loadTransactions(apiURL) {
+
+    const initialNrRows = 5;
+
+    fetch(apiURL)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          transactions: result,
+          nrRows: Math.min(initialNrRows, result.length),
+          hideShowMore: result.length <= initialNrRows
+        });
+      },
+
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      }
+    )
+  }
+
+  handleShowMore() {
+    const rowIncrement = 10;
+
+    this.setState(prevState => ({
+        nrRows: Math.min(prevState.nrRows + rowIncrement, prevState.transactions.length),
+        hideShowMore: (prevState.nrRows + rowIncrement) >= prevState.transactions.length
+    }));
+  }
+
+  componentDidMount() {
+    this.loadTransactions(this.props.apiURL)
+  }
+
+  render() {
+    
+    const anyTransactions = this.state.transactions.length !== 0;
+
+    const TableAndButtons = ({ visible }) => {
       if (!visible) {
-        return <div/>
+        return <div />
       }
       return (
-        <div>
+        <div class="w-full">
           <div class="w-full justify-center">
             <Table
-              content={transactions}
+              content={this.state.transactions}
               nrRows={this.state.nrRows}
               keyHeader="id"
               clickable={true}
@@ -243,59 +219,42 @@ class OpenTransactionsTable extends React.Component {
           </div>
 
           <div class="py-6 flex">
-            <div class="w-1/3"/>
+            <div class="w-1/3" />
             <div class="w-1/3">
-              <div class="flex justify-center">
+              <div class={"flex justify-center " + (this.state.hideShowMore ? "hidden" : "")}>
                 <ShowMoreButton onClick={() => this.handleShowMore()} />
               </div>
             </div>
-            <div class="w-1/3 h-12 flex justify-end">
-              <Button
-                text="Add Transaction"
-                main={true}
-                handleClick={() => this.setModalOpen(true)}
-              />
-            </div>
+            {this.props.bottomRightContent}
           </div>
         </div>
       )
     }
 
-    const MissingAndButton = ({visible}) => {
-      if (!visible) {
-        return <div/>
-      }
-      return (
-          <div class="flex justify-center">
-            <div>
-              <h4 class="text-2xl font-semibold text-gray-400">
-                You don't have any open transactions
-              </h4>
-              <div class="flex justify-center py-2">
-                <Button
-                  text="Add Transaction"
-                  main={true}
-                  handleClick={() => this.setModalOpen(true)}
-                />
-              </div>
-            </div>
-          </div>
-      )
+    if (this.state.navigate) {
+      return <Navigate to={this.state.navigateTo} push={true} />
     }
 
+    if (this.state.error) {
+      return <div> Error; {this.state.error.message} </div>;
+    }
+
+    if (!this.state.isLoaded) {
+      return (
+        <div>
+          <ClipLoader loading={!this.state.isLoaded} size={150} />
+        </div>
+      )
+    }
     return (
       <div class="w-full">
-        <TableAndButtons visible={anyTransactions}/>
-        <MissingAndButton visible={!anyTransactions}/>
-        <Modal
-          open={this.state.modalOpen}
-          setOpen={(bool) => this.setModalOpen(bool)}
-          loadTransactions={() => this.loadTransactions()}
-        />
+        <TableAndButtons visible={anyTransactions} />
+        {anyTransactions ? null : this.props.NoTransactionContent}
       </div>
     )
-
   }
+
 }
+
 
 export default Transactions;
