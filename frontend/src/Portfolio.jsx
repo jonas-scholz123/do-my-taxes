5a961598560b5046345ce6f37bc13479d321fe1f
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-07-13 22:07:55 +0100

    Cleanup + replace old portfolio page

--- frontend/src/Portfolio.jsx
+++ frontend/src/Portfolio.jsx
@@ -1,43 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "./components/Header";
 import Card from "./components/Card";
 import BarChart from "./portfolio/BarChart";
 import HistoryBrowser from './portfolio/HistoryBrowser';
 import PortfolioTable from './portfolio/PortfolioTable';
-import PositionCard from './portfolio/PositionCard';
+import ApiWrapper from './components/ApiWrapper'
+import PositionCards from './portfolio/PositionCards'
 
-export default function Portfolio() {
-
+export default function Portfolio(props) {
   return (
-    <div class="h-screen">
+    <div>
       <Header active="Portfolio" />
-      <div class="flex justify-center">
-        <div class="w-10/12">
-          {/*<p class="text-5xl font-bold p-6">Your Portfolio</p>*/}
-          <div class="flex justify-center">
-            <div class="w-1/3">
-              <Card classes="my-4 py-1 p-1" content={<PositionCard/>}/>
-            </div>
-            <div class="w-2/3">
-              <Card content={
-                <HistoryBrowser
-                  apiURL="http://localhost:5000/api/portfolio/history"
-                />}
-                classes="px-6 my-4 ml-4"
-              />
-            </div>
-
-          </div>
-          <div>
-            <PortfolioTable
-              apiURL="http://localhost:5000/api/portfolio/snapshot/now"
-            />
-          </div>
-          <div class="h-64 w-full">
-            <BarChart />
+      <div class="h-screen grid grid-cols-12 gap-5 p-5">
+        <div class="col-span-4 overflow-scroll border rounded-lg shadow-lg ">
+          <div class="grid grid-cols-1 divide-y">
+            <h1 class="text-3xl font-bold p-4"> Open Positions</h1>
+            <PositionCards/>
           </div>
         </div>
-
+        <div class="col-span-8 overflow-scroll border rounded-lg shadow-lg">
+          <div class="p-4 bg-white">
+            <h1 class="text-3xl font-bold pb-3"> Portfolio History </h1>
+            <HistoryBrowser
+              apiURL="http://localhost:5000/api/portfolio/history"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
