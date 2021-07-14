3dd485b7bd1d45ba3aab7ca785ac25402e38238c
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-07-14 15:44:21 +0100

    remove unnecessary imports

--- frontend/src/Portfolio.jsx
+++ frontend/src/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from "./components/Header";
 import HistoryBrowser from './portfolio/HistoryBrowser';
 import PositionCards from './portfolio/PositionCards'
