6d8667cf5f15882e1ce7c8b42edeed3ae4252d79
Jonas Scholz <jonas.scholz123@googlemail.com>
2021-07-14 14:50:36 +0100

    4 space tabs -> 2 space tabs

--- frontend/src/portfolio/HistoryBrowser.jsx
+++ frontend/src/portfolio/HistoryBrowser.jsx
@@ -3,120 +3,120 @@ import AreaChart from './AreaChart'
 import Button from '../components/BaseButton';
 import withApiWrapper from '../components/ApiWrapper';
 
-const d3 = require('d3-array'); 
+const d3 = require('d3-array');
 
 
 class HistoryBrowser extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            from: new Date(2019, 1, 1),
-            to: new Date(),
-            nrItems: 100,
-            activeButton: "Max",
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      from: new Date(2019, 1, 1),
+      to: new Date(),
+      nrItems: 100,
+      activeButton: "Max",
     }
+  }
 
-    convertTimestamp(timestamp) {
-        return new Date(timestamp).toISOString().substring(0, 10);
+  convertTimestamp(timestamp) {
+    return new Date(timestamp).toISOString().substring(0, 10);
+  }
+
+  dateISO(date) {
+    return date.toISOString().substring(0, 10)
+  }
+
+  reshapeData(data) {
+    var newData = [];
+    for (const [category, catData] of Object.entries(data)) {
+      var lineDict = { "id": category };
+      var lineData = [];
+      for (const [x, y] of Object.entries(catData)) {
+        lineData.push({ "x": this.convertTimestamp(parseInt(x)), "y": y })
+      }
+      lineDict["data"] = lineData;
+      newData.push(lineDict);
     }
+    return newData;
+  }
 
-    dateISO(date) {
-        return date.toISOString().substring(0, 10)
+  changeFromDate(daysAgo, activeName) {
+    if (daysAgo === null) {
+      this.setState({
+        from: "earliest",
+        activeButton: activeName,
+      })
+      return
     }
+    var from = new Date();
+    from.setDate(from.getDate() - daysAgo)
+    // reload
+    this.setState({
+      from: from,
+      activeButton: activeName,
+    })
+  }
 
-    reshapeData(data) {
-        var newData = [];
-        for (const [category, catData] of Object.entries(data)) {
-            var lineDict = {"id": category};
-            var lineData = [];
-            for (const [x, y] of Object.entries(catData)) {
-                lineData.push({"x": this.convertTimestamp(parseInt(x)), "y": y})
-            }
-            lineDict["data"] = lineData;
-            newData.push(lineDict);
-        }
-        return newData;
+  truncateData(data, truncationIndex) {
+    return data.map(
+      (curve) => ({
+        id: curve["id"],
+        data: curve["data"].slice(truncationIndex),
+      })
+    )
+  }
+
+  getShownData() {
+    var data;
+    const history = this.reshapeData(this.props.data);
+    if (this.state.from !== "earliest") {
+      const truncationIndex = d3.bisectLeft(history[0]["data"].map(e => e.x), this.dateISO(this.state.from));
+      data = this.truncateData(history, truncationIndex);
     }
-
-    changeFromDate(daysAgo, activeName){
-        if (daysAgo === null) {
-            this.setState({
-                from: "earliest",
-                activeButton: activeName,
-            })
-            return
-        }
-        var from = new Date();
-        from.setDate(from.getDate() - daysAgo)
-        // reload
-        this.setState({
-            from: from,
-            activeButton: activeName,
-        })
+    else {
+      data = this.props.data;
     }
+    return data
+  }
 
-    truncateData(data, truncationIndex){
-        return data.map(
-            (curve) => ({
-                id: curve["id"],
-                data: curve["data"].slice(truncationIndex),
-                })
-        )
-    }
+  getButtonArray() {
+    const buttonValues = [
+      { name: "Month", days: 30 },
+      { name: "Year", days: 365 },
+      { name: "Max", days: null },
+    ]
 
-    getShownData() {
-        var data;
-        const history = this.reshapeData(this.props.data);
-        if (this.state.from !== "earliest") {
-            const truncationIndex = d3.bisectLeft(history[0]["data"].map(e => e.x), this.dateISO(this.state.from));
-            data = this.truncateData(history, truncationIndex);
-        }
-        else {
-            data = this.props.data;
-        }
-        return data
-    }
+    return buttonValues.map(
+      (val) =>
+        <Button
+          text={val.name}
+          handleClick={() => (this.changeFromDate(val.days, val.name))}
+          active={val.name === this.state.activeButton}
+          activeClasses="border-indigo-700 "
+        />
+    )
+  }
 
-    getButtonArray() {
-        const buttonValues = [
-            {name: "Month", days: 30},
-            {name: "Year", days: 365},
-            {name: "Max", days: null},
-        ]
+  render() {
 
-        return buttonValues.map(
-            (val) =>
-                <Button
-                    text={val.name}
-                    handleClick={() => (this.changeFromDate(val.days, val.name))}
-                    active={val.name === this.state.activeButton}
-                    activeClasses="border-indigo-700 "
-                />
-        )
-    }
+    const data = this.getShownData();
+    const buttons = this.getButtonArray();
 
-    render() {
-
-        const data = this.getShownData();
-        const buttons = this.getButtonArray();
-
-        return (
-            <div class="">
-                <div class="h-96">
-                    <AreaChart
-                        data={data}
-                    />
-                </div>
-                <div class="flex justify-end">
-                    <div class="w-1/4 flex justify-end">
-                        {buttons}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div class="">
+        <div class="h-96">
+          <AreaChart
+            data={data}
+          />
+        </div>
+        <div class="flex justify-end">
+          <div class="w-1/4 flex justify-end">
+            {buttons}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default withApiWrapper(HistoryBrowser, "http://localhost:5000/api/portfolio/history");
